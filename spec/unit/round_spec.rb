07dfb7fb92a7fbb2aed949f708f58f0c511ea494
Blaine Lafreniere <blafreniere@deseretbook.com>
2023-03-24 20:08:28 -0600

    add stub specs for round

--- spec/unit/round_spec.rb
+++ spec/unit/round_spec.rb
@@ -1,2 +1,11 @@
 RSpec.describe Round do
+  describe '#start' do
+    it 'deals cards to all players'
+    it 'prompt an initial round of betting'
+    it 'prompt players to discard and re-draw up to 3 cards, or stand pat'
+    it 'prompt a second round of betting'
+    it 'reveal hands'
+    it 'evaluate the value of each hand'
+    it 'determine who won'
+  end
 end
